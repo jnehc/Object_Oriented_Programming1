3d694e235a86531c0a75cf959d06cbda14d07dbe
jhc <coombsjh@gmail.com>
2015-01-25 01:00:39 -0500

    initial planning

--- rover.rb
+++ rover.rb
@@ -1,35 +1,5 @@
 class Rover
-def initialize
-		@xcord = xcord
-		@ycord = ycord
-		@direction = direction
-	end
-# location (x,y) and direction
-
-#how to define N E S W directions, 4 quadrants 
-	
-	# needs to have 3 behaviors: move, turn, read
-	def move
-	end
-
-	def turn
-	end
-
-	def instruct
-	end
-
-end
-
-#make an instance of rover starting at (1,2,N) and move it 
-# 'L' and 'R' makes the rover spin 90 degrees left or right respectively, without moving from its current spot.
-# 'M' means move forward one grid point, and maintain the same heading.
-# Assume that the square directly North from (x, y) is (x, y+1).
-
-
-class Rover
-attr_accessor :xcord
-attr_accessor :ycord
-attr_accessor :direction
+attr_accessor :xcord :ycord :direction
 
 def initialize
      @xcord = xcord
@@ -37,39 +7,64 @@ def initialize
      @direction = direction
 end
 
-states:
-location (xcord,ycord)
-orientation (N W E S)
+# method for location (x,y) and direction
+# get currentlocation
+# get direction
 
-behaviours:
-move +1
-turn (left/right)
-
---
-MOVE
-make all into integers?
-
-board is 5 x5 how to demonstrate this?
-old_location = (x,y)  
-
-
-#{xcord}, #{ycord} = new_location
-direction = new_location
-
-
-
-case @direction
-  when "N" ## ycord + 1
-  when "S" ##ycord - 1
-  when "W" ##xcord-1
-  when "E" ##xcord+1
-  
-
- else  ##nothing##
+def takeoff (location)          
+      loc_array = location.split("")
+      @x = loc_array[0].to_i
+      @y = loc_array[1].to_i
+      @direction = loc_array[2]
 end
 
-def move
-moved.each { |existing|do something } 
+	def move
+	end
+
+	def turn
+		if 
+	end
+
+	def instruct
+	end
+
+
+
+#make an instance of rover starting at (1,2,N) and move it 
+# 'L' and 'R' makes the rover spin 90 degrees left or right respectively, without moving from its current spot.
+# 'M' means move forward one grid point, and maintain the same heading.
+# Assume that the square directly North from (x, y) is (x, y+1).
+
+# states:
+# location (xcord,ycord)
+# orientation (N W E S)
+
+
+
+
+
+# case @direction
+
+#   when "N" ## ycord + 1
+#   when "S" ##ycord - 1
+#   when "W" ##xcord-1
+#   when "E" ##xcord+1
+ 
+ # when "N" 
+ # 	ycord + 1
+ # when "S"
+ # 	ycord - 1
+ # when "W"
+ # 	xcord - 1
+ # when "E"
+ # 	xcord + 1
+
+
+
+
+
+
+
 
 
 
@@ -82,7 +77,12 @@ TURN
 Turn Options: N =north, W=west, E=east, S=south
 directions = [N, W, E, S]
 
-
+end
 #create new instance variables from Rover
-roverman = Rover.new
-roverlady = Rover.new
+roverman = Rover.new(1,2,N)
+#roverman starting values xcord=1,ycord2, direction=N  
+roverlady = Rover.new(3,3,E)
+#roverlady starting  values xcord=3,ycord3, direction=E
+#move intance around
+#roverman record final coordinates 3-1-N
+#roverlady xecord final coordinates 4-2-N
